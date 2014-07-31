84843befd6f3623390b482e61454e0ed2693f54b
hiyamamo <h1r0ki_yamam0t0@yahoo.co.jp>
2014-07-31 22:44:33 +0900

    シンボリックリンク作成先が間違っていた

--- create_ln.sh
+++ create_ln.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vim ~/vim
+ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
